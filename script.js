9c72bd47c8b675a86b5f7603e1a7a6560138ba55
Akanksha101999 <97754410+Akanksha101999@users.noreply.github.com>
2023-06-20 18:51:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
+	let strJoin = str.split('').join('').tolowerCase();
+	let isPalindrome = true;
+	let i = 0; j = strJoin.length-1;
+	while(i <= j && isPallindrome){
+		if(strJoin[i] !== strjoin[j]){
+			isPalindrome = false;
+		}
+		i++;
+		j--;
+	}
+	return isPalindrome;
 }
 module.exports = palindrome
